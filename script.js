4d2fa2cd13d4cb3de3fd3adfc499a587db6445bb
AbbasKazmi <abbaskazmi2005@gmail.com>
2022-05-06 11:30:37 -0400

    20

--- script.js
+++ script.js
@@ -1,3 +1,1 @@
-var x = document.querySelector('div#wg-api-football-fixtures')
-
-x.setAttribute('data-key', apiKey);
+document.querySelector('div#wg-api-football-fixtures').setAttribute('data-key', apiKey);
