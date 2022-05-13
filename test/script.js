9fa662b61faa809cd4423ba06fd2ad493864c86e
AbbasKazmi <abbaskazmi2005@gmail.com>
2022-05-13 08:19:21 -0400

    b

---
+++ test/script.js
@@ -0,0 +1,16 @@
+console.log('test')
+
+const settings = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://v3.football.api-sports.io/widgets/livescore?date=2021-04-07",
+	"method": "GET",
+	"headers": {
+		"X-RapidAPI-Host": "v3.football.api-sports.io",
+		"X-RapidAPI-Key": "e54f3d3972ca8251c1259694b49948de"
+	}
+};
+
+$.ajax(settings).done(function (response) {
+	console.log(response);
+});
