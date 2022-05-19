261697e0ac16f8b9821841a0b843398f2eabb2b9
Footballify <abbaskazmi2005@gmail.com>
2022-05-18 21:42:33 -0400

    uu8u8u8u

--- test/script.js
+++ test/script.js
@@ -23,10 +23,6 @@ var run = async () => {
     console.log(filtered);
 
 	for (i = 0; i < filtered.length-1; i++) {
-        
-		console.log(filtered[i].league.name)
-		console.log(filtered[i].teams.home.name, filtered[i].goals.home)
-		console.log(filtered[i].teams.away.name, filtered[i].goals.away)
 
 
         let parent = document.createElement("div")
