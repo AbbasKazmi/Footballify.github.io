5d8dbe811fcdb41a45419e4d2b2c442677703043
Footballify <abbaskazmi2005@gmail.com>
2022-05-21 11:39:03 -0400

    dascsc

--- test/script.js
+++ test/script.js
@@ -98,18 +98,31 @@ var run = async () => {
             awayTeamScore.innerHTML = 0
             awayTeamScore.classList.add('hide')
 
-        } else if (String(filtered[i][x].fixture.status.short) === 'FT' || 'HT') {
+        } else if (String(filtered[i][x].fixture.status.short) === 'FT') {
             homeTeamScore.classList.remove('hide')
             homeTeamScore.innerHTML = filtered[i][x].goals.home
             awayTeamScore.classList.remove('hide')
             awayTeamScore.innerHTML = filtered[i][x].goals.away
-        } else if (String(filtered[i][x].fixture.status.short) == '1H' || '2H') {
+        } else if (String(filtered[i][x].fixture.status.short) === 'HT') {
+            homeTeamScore.classList.remove('hide')
+            homeTeamScore.innerHTML = filtered[i][x].goals.home
+            awayTeamScore.classList.remove('hide')
+            awayTeamScore.innerHTML = filtered[i][x].goals.away
+        } else if (String(filtered[i][x].fixture.status.short) == '1H') {
                 homeTeamScore.classList.remove('hide')
                 homeTeamScore.innerHTML = filtered[i][x].goals.away
                 awayTeamScore.classList.remove('hide')
                 awayTeamScore.innerHTML = filtered[i][x].goals.away
                 console.log(homeTeamName)
                 gameStatus.innerHTML = filtered[i][x].fixture.status.elapsed
+        } else if (String(filtered[i][x].fixture.status.short) == '2H') {
+            homeTeamScore.classList.remove('hide')
+            homeTeamScore.innerHTML = filtered[i][x].goals.away
+            awayTeamScore.classList.remove('hide')
+            awayTeamScore.innerHTML = filtered[i][x].goals.away
+            console.log(homeTeamName)
+            gameStatus.innerHTML = filtered[i][x].fixture.status.elapsed
+        
         } else {}
 
         //If Home Wins
