7a561763eb60cc7f6e5d8871878ac5c775365c71
Footballify <abbaskazmi2005@gmail.com>
2022-05-22 22:31:31 -0400

    knio09809

--- test/script.js
+++ test/script.js
@@ -129,11 +129,6 @@ var run = async () => {
             homeTeamScore.innerHTML = filtered[i][x].goals.home
             awayTeamScore.classList.remove('hide')
             awayTeamScore.innerHTML = filtered[i][x].goals.away
-        } else if (String(filtered[i][x].fixture.status.short) === 'PST') {
-            homeTeamScore.classList.remove('hide')
-            homeTeamScore.innerHTML = filtered[i][x].goals.home
-            awayTeamScore.classList.remove('hide')
-            awayTeamScore.innerHTML = filtered[i][x].goals.away
         } else if (String(filtered[i][x].fixture.status.short) === 'AET') {
             homeTeamScore.classList.remove('hide')
             homeTeamScore.innerHTML = filtered[i][x].goals.home
