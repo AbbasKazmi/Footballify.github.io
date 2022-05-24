48118de4bdb4dea03408a69a7efa05917b55f601
Footballify <abbaskazmi2005@gmail.com>
2022-05-24 10:19:49 -0400

    demkd39

--- test/script.js
+++ test/script.js
@@ -284,44 +284,7 @@ var run = async () => {
             awayTeamLogoRightDiv.className = 'logo1'
             awayTeamLogoRightDiv.setAttribute("src", awayTeamLogo.getAttribute("src"))
             sideScore.appendChild(awayTeamLogoRightDiv)
-            // let month="placeholder"
-            // let timeHTML = timerOrigin.innerHTML.slice(6,7)
-            // console.log(timeHTML)
-            // if (timeHTML = "1") {
-            //     month = "Jan"
-            //     timeHTML="01"
-            // } else if (timerOrigin.innerHTML.slice(6,7) = "2") {
-            //     month = "Feb"
-            //     timeHTML="02"
-            // } else if (timerOrigin.innerHTML.slice(6,7) = "3") {
-            //     month = "Mar"
-            //     timeHTML="03"
-            // } else if (timerOrigin.innerHTML.slice(6,7) = "4") {
-            //     month = "Apr"
-            //     timeHTML="04"
-            // } else if (timerOrigin.innerHTML.slice(6,7) = "5") {
-            //     month = "May"
-            //     timeHTML="05"
-            // } else if (timerOrigin.innerHTML.slice(6,7) = "6") {
-            //     month = "Jun"
-            //     timeHTML="06"
-            // } else if (timerOrigin.innerHTML.slice(6,7) = "7") {
-            //     month = "Jul"
-            //     timeHTML="07"
-            // } else if (timerOrigin.innerHTML.slice(6,7) = "8") {
-            //     month = "Aug"
-            //     timeHTML="08"
-            // } else if (timerOrigin.innerHTML.slice(6,7) = "9") {
-            //     month = "Sep"
-            //     timeHTML="09"
-            // } else if (timerOrigin.innerHTML.slice(6,7) = "10") {
-            //     month = "Oct"
-            // } else if (timerOrigin.innerHTML.slice(6,7) = "11") {
-            //     month = "Nov"
-            // } else if (timerOrigin.innerHTML.slice(6,7) = "12") {
-            //     month = "Dec"
-            // }
-            // var countDownDate = new Date(`${month} + ${timerOrigin.innerHTML.slice(9,10)}, ${timerOrigin.innerHTML.slice(0,4)} ${timerOrigin.innerHTML.slice(12,19)} `).getTime();
+
             let timer = document.createElement("div")
             timer.className = 'timer'
             // Update the count down every 1 second
@@ -353,6 +316,7 @@ var run = async () => {
             var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
             if (seconds.length == 1) {
+                console.log('seconds')
                 seconds = "0" + seconds;
             }
                 
@@ -383,14 +347,15 @@ var run = async () => {
 };
 
 
-
+//Run Page
 run();
 
+//Refresh Data
 const interval = setInterval(function() {
     run();
   }, 60000);
 
-
+//Page Load Delay
 $( document ).ready(function() {
     console.log('page loaded')
     setTimeout(function () {
