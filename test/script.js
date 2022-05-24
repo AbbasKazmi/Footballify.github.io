01c9a7a57a7b5609fac3bdc662913643c74dab6a
Footballify <abbaskazmi2005@gmail.com>
2022-05-24 15:28:50 -0400

    ccmxc acm

--- test/script.js
+++ test/script.js
@@ -314,8 +314,8 @@ var run = async () => {
             console.log(`${awayTeamName.innerHTML} Has Been Clicked`)
 
 
-            document.querySelector('.sideScoreDiv').appendChild(teamNames).hide().fadeIn(600);
-            document.querySelector('.sideScoreDiv').appendChild(sideScore).hide().fadeIn(600);
+            document.querySelector('.sideScoreDiv').appendChild(teamNames)
+            document.querySelector('.sideScoreDiv').appendChild(sideScore)
         })
 
         
