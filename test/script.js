f4735102a5c8e7f90a4af3035ce5cd8affc7a440
Footballify <abbaskazmi2005@gmail.com>
2022-05-24 10:30:05 -0400

    0u9iojkl9

--- test/script.js
+++ test/script.js
@@ -295,35 +295,35 @@ var run = async () => {
   
             // Find the distance between now and the count down date
             var distance = Number(String(timerOrigin.innerHTML)+"000") - now;
-            console.log(distance)
   
             // Time calculations for days, hours, minutes and seconds
+
+            //Days are not displayed, only used to help calculate hours
             var days = Math.floor(distance / (1000 * 60 * 60 * 24));
 
 
-            var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)) + (days * 24);
+            let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)) + (days * 24);
             if (hours.length == 1) {
                 hours= "0" + hours;
             }
 
 
-            var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+            let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
             if (minutes.length == 1) {
                 minutes= "0" + minutes;
             }
 
 
-            var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+            let seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
             if (seconds.length == 1) {
-                console.log('seconds')
                 seconds = "0" + seconds;
             }
                 
-            // Display the result in the element with id="demo"
+            // Display the time
             timer.innerHTML = hours + ":" + minutes + ":" + seconds;
   
-            // If the count down is finished, write some text
+            // If the count down is finished, Display score
             if (distance < 0) {
             clearInterval(x);
             timer.innerHTML = homeTeamScore.innerHTML + "-" + awayTeamScore.innerHTML;
