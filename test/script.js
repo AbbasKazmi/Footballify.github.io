9f71717db5fa698dfeabbd2488a0506573376316
Footballify <abbaskazmi2005@gmail.com>
2022-05-24 08:47:48 -0400

    celcmekmck3

--- test/script.js
+++ test/script.js
@@ -270,15 +270,15 @@ var run = async () => {
             homeTeamLogoRightDiv.setAttribute("src", homeTeamLogo.getAttribute("src"))
             sideScore.appendChild(homeTeamLogoRightDiv)
 
-            let homeTeamScoreRightDiv = document.createElement("div")
-            homeTeamScoreRightDiv.className = 'fixtureScoreRight'
-            homeTeamScoreRightDiv.innerHTML = homeTeamScore.innerHTML + "-" 
-            sideScore.appendChild(homeTeamScoreRightDiv)
+            // let homeTeamScoreRightDiv = document.createElement("div")
+            // homeTeamScoreRightDiv.className = 'fixtureScoreRight'
+            // homeTeamScoreRightDiv.innerHTML = homeTeamScore.innerHTML + "-" 
+            // sideScore.appendChild(homeTeamScoreRightDiv)
 
-            let awayTeamScoreRightDiv = document.createElement("div")
-            awayTeamScoreRightDiv.className = 'fixtureScoreLeft'
-            awayTeamScoreRightDiv.innerHTML = awayTeamScore.innerHTML
-            sideScore.appendChild(awayTeamScoreRightDiv)
+            // let awayTeamScoreRightDiv = document.createElement("div")
+            // awayTeamScoreRightDiv.className = 'fixtureScoreLeft'
+            // awayTeamScoreRightDiv.innerHTML = awayTeamScore.innerHTML
+            // sideScore.appendChild(awayTeamScoreRightDiv)
 
             let awayTeamLogoRightDiv = document.createElement("img")
             awayTeamLogoRightDiv.className = 'logo1'
@@ -323,10 +323,34 @@ var run = async () => {
             }
             var countDownDate = new Date(`${month} + ${timerOrigin.innerHTML.slice(9,10)}, ${timerOrigin.innerHTML.slice(0,4)} ${timerOrigin.innerHTML.slice(12,19)} `).getTime();
             console.log(countDownDate)
-            
+
             let timer = document.createElement("div")
             timer.className = 'timer'
-            timer.innerHTML = 
+            // Update the count down every 1 second
+            var x = setInterval(function() {
+
+            // Get today's date and time
+            var now = new Date().getTime();
+  
+            // Find the distance between now and the count down date
+            var distance = countDownDate - now;
+  
+            // Time calculations for days, hours, minutes and seconds
+            var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+            var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+            var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+                
+            // Display the result in the element with id="demo"
+            timer.innerHTML = days + "d " + hours + "h "
+            + minutes + "m " + seconds + "s ";
+  
+            // If the count down is finished, write some text
+            if (distance < 0) {
+            clearInterval(x);
+            timer.innerHTML = homeTeamScore.innerHTML + "-" + awayTeamScore.innerHTML;
+            }
+        }, 1000);
             sideScore.appendChild(timer)
 
             console.log(`${awayTeamName.innerHTML} Has Been Clicked`)
