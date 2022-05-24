b2e793ab98ecc29e32fb9d40840d9e4022cd78b3
Footballify <abbaskazmi2005@gmail.com>
2022-05-24 09:58:30 -0400

    g5r6tyg

--- test/script.js
+++ test/script.js
@@ -339,8 +339,18 @@ var run = async () => {
             // Time calculations for days, hours, minutes and seconds
             var days = Math.floor(distance / (1000 * 60 * 60 * 24));
             var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)) + (days * 24);
+            if (hours.length == 1) {
+                hours= "0" + currentMinutes;
+            }
             var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+            if (minutes.length == 1) {
+                minutes= "0" + currentMinutes;
+            }
             var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+            if (seconds.length == 1) {
+                seconds= "0" + currentMinutes;
+            }
                 
             // Display the result in the element with id="demo"
             timer.innerHTML = hours + ":" + minutes + ":" + seconds;
