7cc78387e2841beb7cb4f7fa09cc50a6cb1452cd
Footballify <abbaskazmi2005@gmail.com>
2022-05-24 15:02:46 -0400

    drtcygv767

--- test/script.js
+++ test/script.js
@@ -314,7 +314,7 @@ var run = async () => {
             console.log(`${awayTeamName.innerHTML} Has Been Clicked`)
 
 
-            $('.sideScoreDiv').fadeIn(600,function(){document.querySelector('.sideScoreDiv').appendChild(teamNames);
+            $('.sideScoreDiv').hide().fadeIn(600,function(){document.querySelector('.sideScoreDiv').appendChild(teamNames);
                                                     document.querySelector('.sideScoreDiv').appendChild(sideScore);
         })
 
