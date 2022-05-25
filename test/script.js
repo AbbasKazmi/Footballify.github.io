65b34eac7096d630ad3e0e424dc80fa72b05ff4c
Footballify <abbaskazmi2005@gmail.com>
2022-05-25 12:07:58 -0400

    djnc9i2wk

--- test/script.js
+++ test/script.js
@@ -404,7 +404,7 @@ var run = async () => {
                     }
                 };
                 
-                fetch(`https://v3.football.api-sports.io/v3/fixtures/lineups?fixture=${fixtureId.innerHTML}`, options)
+                fetch(`https://v3.football.api-sports.io/fixtures/lineups?fixture=${fixtureId.innerHTML}`, options)
                     .then(response => response.json())
                     .then(response => console.log(response))
                     .catch(err => console.error(err));
