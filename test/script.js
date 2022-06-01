2e61fec12b5e3fec9395ac51e316e7e366801ff3
Footballify <abbaskazmi2005@gmail.com>
2022-06-01 16:28:20 -0400

    dck m993

--- test/script.js
+++ test/script.js
@@ -143,7 +143,7 @@ var run = async () => {
 
         let textstart11 = document.createElement('div')
         textstart11.className = 'startEleven'
-        if (lineupData[0].formation) {
+        if (lineupData[0].formation!=null || lineupData[0].formation!=undefined) {
             textstart11.innerHTML = 'Starting XI'
             console.log(lineupData[0].formation)
             document.querySelector('.sideScoreDiv').appendChild(textstart11)
