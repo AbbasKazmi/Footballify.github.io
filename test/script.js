b24ef3b7df741aa3924811919c4287ea0dc0f90f
Footballify <abbaskazmi2005@gmail.com>
2022-06-01 16:53:52 -0400

    ekodmc303939

--- test/script.js
+++ test/script.js
@@ -144,6 +144,10 @@ var run = async () => {
         let textstart11 = document.createElement('div')
         textstart11.className = 'startEleven'
 
+        if (lineupData[0].formation==undefined) {
+            console.log('undef')
+        } else {
+
         if (lineupData[0].formation) {
             textstart11.innerHTML = 'Starting XI'
             document.querySelector('.sideScoreDiv').appendChild(textstart11)
@@ -173,7 +177,7 @@ var run = async () => {
 
         }
 
-       
+    }
 
     document.querySelector('.sideScoreDiv').appendChild(lineupC)
     }
