e08e973b8c13c0b1c67e1d71de8dd133b4accd65
Footballify <abbaskazmi2005@gmail.com>
2022-06-02 14:57:48 -0400

    gjhb78

--- test/script.js
+++ test/script.js
@@ -331,8 +331,8 @@ for (let d=eventsData.length-1; d>=0; d--) {
                 
     let eventOccured = document.createElement('div')
     eventOccured.classList = "lineupParentHome"
-
-    if (homeTm=eventsData[d].team.name) {
+    
+    if (homeTm==eventsData[d].team.name) {
         console.log('homew')
         if (eventsData[d].type=="Goal") {
         eventOccured.innerHTML=`<img class='ball' src="https://www.citypng.com/public/uploads/small/11649467416xzjfid709wwfnn4b4minvcmsdpiyjrajom2djrhvdh5r1fybjfrf2rsp7vq2bc6ujsij9nsb9jfznh2pvoofx8uziapv9ekhjexe.png">` + eventsData[d].time.elapsed + "' | " + eventsData[d].player.name
