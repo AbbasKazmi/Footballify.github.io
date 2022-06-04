9ee826f25edb39e8872428dd43662fe3e8158f66
Footballify <abbaskazmi2005@gmail.com>
2022-06-04 11:43:19 -0400

    okmmk0ok

--- script.js
+++ script.js
@@ -543,7 +543,8 @@ if (eventsData[d].time.extra>0) {
         } else if (String(filtered[i][x].fixture.status.short) == '1H') {homeTeamScore.classList.remove('hide');homeTeamScore.classList.add('live');homeTeamScore.innerHTML = filtered[i][x].goals.home;awayTeamScore.classList.remove('hide');awayTeamScore.classList.add('live');awayTeamScore.innerHTML = filtered[i][x].goals.away;gameStatus.classList.add('live');gameStatus.innerHTML = filtered[i][x].fixture.status.elapsed + "′";
         } else if (String(filtered[i][x].fixture.status.short) == 'ET') {homeTeamScore.classList.remove('hide');homeTeamScore.classList.add('live');homeTeamScore.innerHTML = filtered[i][x].goals.home;awayTeamScore.classList.remove('hide');awayTeamScore.classList.add('live');awayTeamScore.innerHTML = filtered[i][x].goals.away;gameStatus.classList.add('live');gameStatus.innerHTML = filtered[i][x].fixture.status.elapsed + "′";
         } else if (String(filtered[i][x].fixture.status.short) == '2H') {homeTeamScore.classList.remove('hide');homeTeamScore.classList.add('live');homeTeamScore.innerHTML = filtered[i][x].goals.home;awayTeamScore.classList.remove('hide');awayTeamScore.classList.add('live');awayTeamScore.innerHTML = filtered[i][x].goals.away;gameStatus.classList.add('live');gameStatus.innerHTML = filtered[i][x].fixture.status.elapsed + "′";} else {}
-
+        
+        let clock = filtered[i][x].fixture.status.elapsed 
         parent.addEventListener("click", function(){
 
                 $('.navbarMini').remove()
@@ -645,7 +646,7 @@ if (eventsData[d].time.extra>0) {
                     
                     let gameTicker = document.createElement('div')
                     gameTicker.className="gameTicker"
-                    console.log(filtered[i][x].fixture.status.elapsed)
+                    console.log(clock)
                     
 
                     if (gameStatus.innerHTML=="FT" || gameStatus.innerHTML=="AWD") {
